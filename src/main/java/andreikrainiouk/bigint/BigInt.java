0d302819cc3466b240d8c56ffed68d504911a950
braincrumbler <andreikrainiouk@gmail.com>
2016-03-28 22:48:21 -0700

    Latest

--- src/main/java/andreikrainiouk/bigint/BigInt.java
+++ src/main/java/andreikrainiouk/bigint/BigInt.java
@@ -3,10 +3,6 @@ package andreikrainiouk.bigint;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Created by Andrei on 3/24/2016.
- */
-
 public class BigInt {
 
     //ArrayList representation of a natural number
@@ -14,7 +10,17 @@ public class BigInt {
     //Denotes sign
     private final boolean positive;
 
-    //Constructor function
+    //Constructor from list
+    public BigInt(List<Integer> value, boolean positive) {
+        if (!positive) {
+            this.positive = false;
+        } else {
+            this.positive = true;
+        }
+        this.value = value;
+    }
+
+    //Constructor from int
     public BigInt(int value) {
         if (value < 0) {
             positive = false;
@@ -34,6 +40,16 @@ public class BigInt {
         return newList;
     }
 
+    //Creates copy of given BigInt with opposite sign
+    public BigInt negCopy() {
+        int thisValue = 0;
+        for (int i = 0; i < this.value.size(); i++) {
+            thisValue += this.value.get(i) * IntHelper.intPow(10, i);
+        }
+        if (this.positive) return new BigInt(thisValue * -1);
+        else return new BigInt(thisValue);
+    }
+
     //Prints out BigInt
     public void printBigInt() {
         if (!positive) System.out.print("-");
@@ -43,25 +59,33 @@ public class BigInt {
         System.out.println();
     }
 
-    //Compares 2 BigInts, returns -1 for this < that, 0 for this = that, and 1 for this > that, 2 for error
+    //Compares 2 BigInts, returns -1 for this < that, 0 for this = that, and 1 for this > that,
+    //and 2 for error
     public int compareBigInt(BigInt that) {
+        //Cases for opposite signs
         if (this.positive && !that.positive) return 1;
         if (!this.positive && that.positive) return -1;
+        //Cases for positive signs
         if (this.positive && that.positive) {
+            //Cases of differing number of digits
             if (this.value.size() > that.value.size()) return 1;
             else if (this.value.size() < that.value.size()) return -1;
             else {
+                //Digit by digit comparison
                 for (int i = this.value.size() - 1; i >= 0; i++) {
                     if (this.value.get(i) > that.value.get(i)) return 1;
                     else if (this.value.get(i) < that.value.get(i)) return -1;
                 }
                 return 0;
             }
+            //Cases for negative signs
         }if (!this.positive && !that.positive) {
+            //Cases of differing number of digits
             if (this.value.size() < that.value.size()) return 1;
-            else if (this.value.size() < that.value.size()) return -1;
+            else if (this.value.size() > that.value.size()) return -1;
             else {
-                for (int i = this.value.size() - 1; i >= 0; i++) {
+                //Digit by digit comparison
+                for (int i = this.value.size() - 1; i >= 0; i--) {
                     if (this.value.get(i) < that.value.get(i)) return 1;
                     else if (this.value.get(i) < that.value.get(i)) return -1;
                 }
@@ -70,18 +94,52 @@ public class BigInt {
         } else return 2;
     }
 
-    //Externally called add function
+    //Externally called add function, runs sum function for largerInput.sum(smallerInput)
     public BigInt add(BigInt that) {
-        return that;
+        if (this.compareBigInt(that) == 0) return this.multiply(2);
+        else if (this.compareBigInt(that) == 1) return this.sum(that);
+        else return that.sum(this);
     }
 
     //Externally called subtraction function
     public BigInt subtract(BigInt that) {
-        return that;
+        return this.add(that.negCopy());
     }
 
     //Called by add and subtract, actual code that calculates sums
     private BigInt sum(BigInt that) {
-        return that;
+        int carry = 0;
+        List<Integer> total = new ArrayList<Integer>();
+        //For same sign sum
+        if ((this.positive && that.positive) || (!this.positive && !that.positive)){
+            for (int i = 0; i < this.value.size(); i++){
+                //When processing digits only in this
+                if (i > that.value.size()) {
+                    int sum = this.value.get(i) + carry;
+                    assert ((sum % 10 < 10) && (sum % 10 >= 0));
+                    total.add(sum % 10);
+                    carry = sum - (sum % 10);
+                //When processing digits existing in both this and that
+                } else {
+                    int sum = this.value.get(i) + that.value.get(i) + carry;
+                    assert ((sum % 10 < 10) && (sum % 10 >= 0));
+                    total.add(sum % 10);
+                    carry = sum - (sum % 10);
+                }
+            }
+            if (carry != 0) {
+                assert (carry > 0);
+                total.add(carry);
+            }
+            if (this.positive) return new BigInt(total, true);
+            else return new BigInt(total, false);
+        //For opposite sign sums
+        } else {
+            return this;
+        }
+    }
+
+    public BigInt multiply(int factor) {
+        return this;
     }
 }
--- src/test/java/andreikrainiouk/bigint/BigIntTest.java
+++ src/test/java/andreikrainiouk/bigint/BigIntTest.java
@@ -1,10 +1,12 @@
 package andreikrainiouk.bigint;
 
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.*;
-/**
- * Created by Andrei on 3/27/2016.
- */
+
 public class BigIntTest {
     @Test
     public void testPrintBigInt() {
@@ -13,6 +15,36 @@ public class BigIntTest {
         num1.printBigInt();
         System.out.println("Should print out '-1234'");
         BigInt num2 = new BigInt(-1234);
+        num2.printBigInt();
+    }
+
+    @Test
+    public void testBigIntListConstructor() {
+        List<Integer> testList = new ArrayList<Integer>();
+        testList.add(0);
+        testList.add(1);
+        testList.add(2);
+        testList.add(3);
+        BigInt temp = new BigInt(testList, false);
+        System.out.println("Should print '-3210'");
+        temp.printBigInt();
+        temp = new BigInt(testList, true);
+        System.out.println("Should print '3210'");
+        temp.printBigInt();
+    }
+
+    @Test
+    public void testNegCopy() {
+        BigInt num1 = new BigInt(-13);
+        BigInt num2 = num1.negCopy();
+        num1.printBigInt();
+        System.out.println("multiplied by -1 is:");
+        num2.printBigInt();
+        System.out.println();
+        num1 = new BigInt(13);
+        num2 = num1.negCopy();
+        num1.printBigInt();
+        System.out.println("multiplied by -1 is:");
         num2.printBigInt();
     }
 
@@ -51,5 +83,39 @@ public class BigIntTest {
         num1 = new BigInt(10);
         num2 = new BigInt(-10);
         assertEquals(1, num1.compareBigInt(num2));
+    }
+
+    @Test
+    public void testAdd(){
+        BigInt num1 = new BigInt(10);
+        BigInt num2 = new BigInt(20);
+        BigInt num3 = num1.add(num2);
+        System.out.println("Should be '30'");
+        num3.printBigInt();
+        num1 = new BigInt(99);
+        num2 = new BigInt(1);
+        num3 = num1.add(num2);
+        System.out.println("Should be '100'");
+        num3.printBigInt();
+        num1 = new BigInt(9);
+        num2 = new BigInt(8);
+        num3 = num1.add(num2);
+        System.out.println("Should be '17'");
+        num3.printBigInt();
+        num1 = new BigInt(-10);
+        num2 = new BigInt(-20);
+        num3 = num1.add(num2);
+        System.out.println("Should be '-30'");
+        num3.printBigInt();
+        num1 = new BigInt(-99);
+        num2 = new BigInt(-1);
+        num3 = num1.add(num2);
+        System.out.println("Should be '-100'");
+        num3.printBigInt();
+        num1 = new BigInt(-9);
+        num2 = new BigInt(-8);
+        num3 = num1.add(num2);
+        System.out.println("Should be '-17'");
+        num3.printBigInt();
     }
 }
